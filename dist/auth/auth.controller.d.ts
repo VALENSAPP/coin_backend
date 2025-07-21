449d60097f7d94dcab60cc97ea970bd7e249ee65
solutionPr <solutions.provider.dev@gmail.com>
2025-07-21 14:56:17 +0530

    updated

--- dist/auth/auth.controller.d.ts
+++ dist/auth/auth.controller.d.ts
@@ -14,22 +14,22 @@ export declare class AuthController {
     login(body: LoginDto): Promise<{
         access_token: string;
         user: {
-            id: string;
             email: string | null;
             password: string | null;
             googleId: string | null;
             twitterId: string | null;
             walletAddress: string | null;
             registrationType: import(".prisma/client").$Enums.RegistrationType;
-            createdAt: Date;
-            updatedAt: Date;
             phoneNumber: string | null;
             gender: import(".prisma/client").$Enums.Gender | null;
             image: string | null;
             age: number | null;
+            otp: string | null;
+            id: string;
+            createdAt: Date;
+            updatedAt: Date;
             deletedAt: Date | null;
             isDeleted: number;
-            otp: string | null;
             otpExpiresAt: Date | null;
             verifyEmail: number;
         };
