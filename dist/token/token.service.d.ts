222fb18941c3d9d87e8189727b5f32ebe5e401e9
solutionPr <solutions.provider.dev@gmail.com>
2025-09-10 17:09:51 +0530

    token contract address updt

--- dist/token/token.service.d.ts
+++ dist/token/token.service.d.ts
@@ -21,17 +21,17 @@ export declare class TokenService {
         userTokenId: string;
     }>;
     getUserToken(userId: string): Promise<{
-        userId: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
-        initialSupply: string;
-        initialPrice: string;
-        scalingConstant: string;
+        userId: string;
         transactionHash: string;
         tokenAddress: string | null;
         tokenName: string;
         tokenSymbol: string;
+        initialSupply: string;
+        initialPrice: string;
+        scalingConstant: string;
         blockNumber: number;
     }>;
     getTokenInfo(tokenAddress: string): Promise<{
@@ -45,17 +45,17 @@ export declare class TokenService {
     }>;
     getUserTokenWithInfo(userId: string): Promise<{
         tokenInfo: null;
-        userId: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
-        initialSupply: string;
-        initialPrice: string;
-        scalingConstant: string;
+        userId: string;
         transactionHash: string;
         tokenAddress: string | null;
         tokenName: string;
         tokenSymbol: string;
+        initialSupply: string;
+        initialPrice: string;
+        scalingConstant: string;
         blockNumber: number;
     } | {
         tokenInfo: {
@@ -67,17 +67,17 @@ export declare class TokenService {
             followers: any;
             poolBalance: any;
         };
-        userId: string;
         id: string;
         createdAt: Date;
         updatedAt: Date;
-        initialSupply: string;
-        initialPrice: string;
-        scalingConstant: string;
+        userId: string;
         transactionHash: string;
         tokenAddress: string | null;
         tokenName: string;
         tokenSymbol: string;
+        initialSupply: string;
+        initialPrice: string;
+        scalingConstant: string;
         blockNumber: number;
     }>;
 }
