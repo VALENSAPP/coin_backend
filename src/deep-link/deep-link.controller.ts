fe85e29a240a30ea4fc0505a83229f96abd18d96
SolutionPr <solutions.provider.dev@gmail.com>
2026-06-01 19:40:48 +0530

    Add title and paragraph to fallback HTML in DeepLinkController

--- src/deep-link/deep-link.controller.ts
+++ src/deep-link/deep-link.controller.ts
@@ -10,10 +10,11 @@ export class DeepLinkController {
       <html>
         <head>
           <meta charset="utf-8">
+          <title>Opening Valens...</title>
           <meta name="viewport" content="width=device-width, initial-scale=1">
         </head>
         <body>
-         
+          <p>Opening Valens...</p>
 
           <script>
             window.location.href = "com.valens.app://${route}/${id}";
