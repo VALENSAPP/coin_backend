1dbe43126025762fc56cc4945dea5a4d0776aa27
SolutionPr <solutions.provider.dev@gmail.com>
2026-06-05 16:18:29 +0530

    Remove default format 'video' from post visibility query in PostService

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1269,7 +1269,6 @@ export class PostService {
       where: {
         isDelete: 'no',
         deletedAt: null,
-        format: 'video',
         type: 'reel',
         AND: [this.buildPostVisibilityWhere(viewerUserId)],
       },
